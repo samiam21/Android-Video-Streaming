1c8a680c51a3b0e8768e5d94f18be1a32d3b347c
Sam Fahnestock <sfahnestock@softwaredesignsolutions.com>
2021-06-16 09:43:00 -0400

    DP-95 - Listen for and alert on wifi state change

--- app/src/main/java/com/sbgsoft/wifivideostream/WiFiDirectBroadcastReceiver.java
+++ app/src/main/java/com/sbgsoft/wifivideostream/WiFiDirectBroadcastReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.widget.Toast;
 
 public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
     private WifiP2pManager mManager;
@@ -21,7 +22,13 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
         String action = intent.getAction();
 
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
 
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                Toast.makeText(context, "WiFi is ON", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(context, "WiFi is OFF", Toast.LENGTH_LONG).show();
+            }
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
 
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {

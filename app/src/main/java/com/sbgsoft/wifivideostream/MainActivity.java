27933a4700e0f7bc95c8bd04e3edd01099be348f
Sam Fahnestock <sfahnestock@softwaredesignsolutions.com>
2021-06-16 09:26:51 -0400

    DP-95 - Adding WiFiDirect structure

--- app/src/main/java/com/sbgsoft/wifivideostream/MainActivity.java
+++ app/src/main/java/com/sbgsoft/wifivideostream/MainActivity.java
@@ -1,12 +1,15 @@
 package com.sbgsoft.wifivideostream;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiEnterpriseConfig;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -16,6 +19,7 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
@@ -27,14 +31,15 @@ import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
     private static Context mContext;
-    private WifiManager.LocalOnlyHotspotReservation mReservation;
-    Button connectButton;
-    Button disconnectButton;
-    Button getIpButton;
-    EditText ssidText;
-    EditText passwordText;
-    TextView outputConsole;
+    Button btnOnOff, btnDiscover, btnSend;
+    ListView listView;
+    EditText writeMsg;
+    TextView read_msg_box, ConnectionStatus;
     WifiManager wifiManager;
+    WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+    BroadcastReceiver mReceiver;
+    IntentFilter mIntentFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,92 +47,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mContext = getApplicationContext();
 
-        connectButton = findViewById(R.id.connectButton);
-        disconnectButton = findViewById(R.id.disconnectButton);
-        getIpButton = findViewById(R.id.getIpButton);
-        ssidText = findViewById(R.id.editTextSSID);
-        passwordText = findViewById(R.id.editTextPassword);
-        outputConsole = findViewById(R.id.editTextOutputConsole);
+        initialize();
 
-        // Initialize the WiFi and WiFi AP manager
-        wifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
-
-        connectButton.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
-            public void onClick(View v) {
-                // Close the keyboard
-                passwordText.onEditorAction(EditorInfo.IME_ACTION_DONE);
-                ssidText.onEditorAction(EditorInfo.IME_ACTION_DONE);
-
-                outputConsole.append("Turning on WiFi Hotspot...\n");
-
-                // Check to make sure we have permissions
-                if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    outputConsole.append("OH NO!");
-                    return;
+        // Enable/Disable WiFi button listener
+        btnOnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (wifiManager.isWifiEnabled()) {
+                    wifiManager.setWifiEnabled(false);
+                    btnOnOff.setText("WiFi On");
+                } else {
+                    wifiManager.setWifiEnabled(true);
+                    btnOnOff.setText("WiFi Off");
                 }
-
-                // TODO: Configure the hotspot info
-
-                // Start up the hotspot
-                wifiManager.startLocalOnlyHotspot(new WifiManager.LocalOnlyHotspotCallback() {
-
-                    @Override
-                    public void onStarted(WifiManager.LocalOnlyHotspotReservation reservation) {
-                        super.onStarted(reservation);
-                        outputConsole.append("WiFi Hotspot is on now!\n");
-                        mReservation = reservation;
-
-                        // Show the connection info
-                        WifiConfiguration currentConfig = mReservation.getWifiConfiguration();
-                        outputConsole.append("SSID: " + currentConfig.SSID + " | PW: " + currentConfig.preSharedKey + "\n");
-
-                        // Get the gateway address
-                        String[] tmp = currentConfig.toString().split("\n");
-                        outputConsole.append(tmp[3] + "\n");
-
-                        Thread socketThread = new Thread(new SocketListener());
-                        socketThread.start();
-                    }
-
-                    @Override
-                    public void onStopped() {
-                        super.onStopped();
-                        outputConsole.append("WiFi Hotspot is off now!\n");
-                    }
-
-                    @Override
-                    public void onFailed(int reason) {
-                        super.onFailed(reason);
-                        outputConsole.append("Failed...\n");
-                    }
-                }, new Handler());
             }
         });
+    }
 
-        disconnectButton.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
-            public void onClick(View v) {
-                if (mReservation != null) {
-                    mReservation.close();
-                }
-                outputConsole.append("WiFi Hotspot is off now!\n");
-            }
-        });
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
+    }
 
-        getIpButton.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
-            public void onClick(View v) {
-                String ip = Formatter.formatIpAddress(wifiManager.getConnectionInfo().getIpAddress());
-                outputConsole.append("IP: " + ip + "\n");
-            }
-        });
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+
+    private void initialize() {
+        btnOnOff = findViewById(R.id.onOff);
+        btnDiscover = findViewById(R.id.discover);
+        btnSend = findViewById(R.id.sendButton);
+        listView = findViewById(R.id.peerListView);
+        writeMsg = findViewById(R.id.writeMsg);
+        read_msg_box = findViewById(R.id.readMsg);
+        ConnectionStatus = findViewById(R.id.connectionStatus);
+
+        wifiManager = (WifiManager)mContext.getSystemService(Context.WIFI_SERVICE);
+        if (wifiManager.isWifiEnabled()) {
+            btnOnOff.setText("WiFi Off");
+        } else {
+            btnOnOff.setText("WiFi Off");
+        }
+
+        mManager = (WifiP2pManager)getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
     }
 }
---
+++ app/src/main/java/com/sbgsoft/wifivideostream/WiFiDirectBroadcastReceiver.java
@@ -0,0 +1,33 @@
+package com.sbgsoft.wifivideostream;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+    private WifiP2pManager mManager;
+    private WifiP2pManager.Channel mChannel;
+    private MainActivity mActivity;
+
+    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel, MainActivity activity) {
+        this.mManager = manager;
+        this.mChannel = channel;
+        this.mActivity = activity;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+
+        }
+    }
+}
